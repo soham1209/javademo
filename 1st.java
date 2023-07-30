56c1fa7adb41b9f2e2373d05082fa9288bb9aa64
soham <sohamns1234@gmail.com>
2023-07-30 20:19:49 +0530

    new change

--- 1st.java
+++
@@ -1,5 +0,0 @@
-public class 1st {
-    int a;
-    string name;
-    
-}
---
+++ a.java
@@ -0,0 +1,21 @@
+public class a {
+    int a;
+    String name;
+
+    public static void main(String[] args) {
+        // declaring variable
+        // creating object
+        int num = 10;// this is variable declaring it is known asprimitive
+        a obj = new a();// this is object with new keyword
+                        // obj is refrance variable and new a() is object
+        a obj1 = new a();
+
+        obj.name = "soham";
+        System.out.println(obj.a);
+        System.out.println(obj.name);
+
+        System.out.println(obj1.a);
+        System.out.println(obj1.name);
+
+    }
+}
